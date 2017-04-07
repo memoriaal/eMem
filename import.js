3e625571f6894f22bcc4b709943a8387f4ec99f0
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2017-04-07 15:46:17 +0300

    add credentials

--- import.js
+++ import.js
@@ -5,6 +5,10 @@ const fs = require('fs')
 
 var first_line_is_for_labels = true
 
+const ES_CREDENTIALS = process.env.ES_CREDENTIALS
+const INDEX = process.env.INDEX
+const SOURCE = process.env.SOURCE
+
 const allikalingid = {
   'r1':     { 'href':'http://www.memento.ee/memento_materjalid/memento_raamatud/memento_r_1.pdf',  'text':'Memento "POLIITILISED ARRETEERIMISED EESTIS 1940-1988' },
   'r2':     { 'href':'http://www.memento.ee/memento_materjalid/memento_raamatud/memento_r_2.pdf',  'text':'Memento "NÕUKOGUDE OKUPATSIOONIVÕIMU POLIITILISED ARRETEERIMISED EESTIS 1940-1988' },
@@ -15,12 +19,21 @@ const allikalingid = {
   'r7':     { 'href':'http://www.memento.ee/memento_materjalid/memento_raamatud/memento_r_7.pdf',  'text':'Memento "NÕUKOGUDE OKUPATSIOONIVÕIMUDE KURITEOD EESTIS KÜÜDITATUD, ARRETEERITUD, TAPETUD 1940-1990 NIMEDE KOONDREGISTER R1 – R6' },
   'r81':    { 'href':'http://www.memento.ee/memento_materjalid/memento_raamatud/memento_r_81.pdf', 'text':'Memento "KOMMUNISMI KURITEOD EESTIS, Lisanimestik 1940–1990, raamatute R1–R7 täiendamiseks' },
   'r81_20': { 'href':'http://www.memento.ee/memento_materjalid/memento_raamatud/memento_r_81.pdf', 'text':'Memento "KOMMUNISMI KURITEOD EESTIS, Lisanimestik 1940–1990, raamatute R1–R7 täiendamiseks' },
+  'mnm':    { 'href':'www.arborit.eu/memento/kommunismiohvrite-memoriaal/Kommunismiohvrite%20memoriaali%20nime%20kirjed%20%2820-03-2017%29%20-%20Stat-Nimek.pdf', 'text':'Memento "KOMMUNISMIOHVRITE MEMORIAALI NIME KIRJETE STATISTIKA' },
+  'okumus': { 'href':'https://okupatsioon.entu.ee', 'text':'Okupatsioonide Muuseumi avalik vaade' },
+}
+
+const convertLinks = function convertLinks(text) {
+  var exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+  var text1 = text.replace(exp, "<a href='$1'>$1</a>");
+  var exp2 =/(^|[^\/])(www\.[\S]+(\b|$))/gim;
+  return text1.replace(exp2, '$1<a target="_blank" href="http://$2">$2</a>');
 }
 
 var csv = require("fast-csv");
 var labels = []
 csv
-  .fromPath("es_export.csv")
+  .fromPath(SOURCE)
   .on("data", function(data) {
     if (first_line_is_for_labels) {
       first_line_is_for_labels = false
@@ -38,7 +51,7 @@ csv
       let spl = kirje.split(':')
       let allikas = spl.shift()
       // console.log(allikas, allikalingid[allikas]);
-      let txt = spl.join(':')
+      let txt = convertLinks(spl.join(':'))
       return {'allikas':allikalingid[allikas], 'kirje':txt}
     })
     save2db(isik, function(error) {
@@ -88,7 +101,7 @@ q.drain = function() {
 
 const save2db = function save2db(isik, callback) {
   let create = {}
-  create.index = 'isikud'
+  create.index = INDEX
   create.type = 'isik'
   create.id = isik.id
   create.body = isik
