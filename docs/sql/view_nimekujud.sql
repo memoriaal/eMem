47ce8b217b10547edf54fc07a0c3a0969abbc182
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2017-12-21 14:45:39 +0200

    EMI nimekujud

--- docs/sql/view_nimekujud.sql
+++ docs/sql/view_nimekujud.sql
@@ -1,10 +1,10 @@
-CREATE OR REPLACE VIEW v_nimekujud AS
+CREATE OR REPLACE VIEW EMI_nimekujud AS
 SELECT *
 FROM EMIR e
 WHERE
    (e.perenimi REGEXP ';' AND e.EmiPerenimi IS NULL)
 OR (e.eesnimi REGEXP ';' AND e.EmiEesnimi IS NULL)
-OR (e.isanimi REGEXP ';' AND e.EmiIsanimi IS NULL)
+-- OR (e.isanimi REGEXP ';' AND e.EmiIsanimi IS NULL)
 OR (e.sünd REGEXP ';' AND e.EmiSünd IS NULL)
 OR (e.surm REGEXP ';' AND e.EmiSurm IS NULL)
 AND id IN (SELECT DISTINCT emi_id FROM kirjed WHERE kivi = '!');
