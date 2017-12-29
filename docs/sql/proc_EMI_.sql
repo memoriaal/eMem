5dfc9c4fdcdbbfdf682b5f5ec437ef7472196e22
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2017-12-29 21:52:38 +0200

    reflow

--- docs/sql/proc_EMI_.sql
+++ docs/sql/proc_EMI_.sql
@@ -1,5 +1,5 @@
 DELIMITER ;;
-CREATE or replace PROCEDURE `EMI_update_id_for`(IN _ik CHAR(10), IN _emi_id INTEGER(11) UNSIGNED)
+CREATE or REPLACE PROCEDURE `EMI_update_id_for`(IN _ik CHAR(10), IN _emi_id INTEGER(11) UNSIGNED)
 BEGIN
     UPDATE kirjed k
     LEFT JOIN seosed s ON s.isikukood2 = k.isikukood
@@ -13,27 +13,39 @@ BEGIN
     ;
 END;;
 
-CREATE or replace PROCEDURE `EMI_create_id_for`(IN _ik CHAR(10), OUT _emi_id INTEGER(11) UNSIGNED)
+CREATE or REPLACE PROCEDURE `EMI_create_id_for`(IN _ik CHAR(10), OUT _emi_id INTEGER(11) UNSIGNED)
 BEGIN
     INSERT INTO EMIR SET id = NULL;
     SELECT last_insert_id() INTO _emi_id;
     CALL EMI_update_id_for(_ik, _emi_id);
 END;;
 
-CREATE or replace PROCEDURE `EMI_consolidate_records`(IN _emi_id INTEGER(11) UNSIGNED)
+CREATE or REPLACE PROCEDURE `EMI_consolidate_records`(IN _emi_id INTEGER(11) UNSIGNED)
 BEGIN
     UPDATE EMIR AS e 
     LEFT JOIN (
       select k.emi_id
-      , group_concat(DISTINCT if(k.perenimi = '', null, upper(k.perenimi)) order by a.prioriteet desc SEPARATOR ';') as perenimi
-      , group_concat(DISTINCT if(k.eesnimi = '', null, replace(upper(k.eesnimi),'ALEKSANDR','ALEKSANDER')) order by a.prioriteet desc SEPARATOR ';') as eesnimi
-      , group_concat(DISTINCT if(
-          k.allikas = 'RK', null, if(k.isanimi = '', null, upper(k.isanimi))
-        ) order by a.prioriteet desc SEPARATOR ';') as isanimi
-      , group_concat(DISTINCT if(k.sünd = '', null, left(k.sünd,4)) order by a.prioriteet desc SEPARATOR ';') as sünd
-      , group_concat(DISTINCT if(k.surm = '', null, left(k.surm,4)) order by a.prioriteet desc SEPARATOR ';') as surm
-      , group_concat(DISTINCT if(k.kommentaar = '', null, k.kommentaar) order by a.prioriteet desc SEPARATOR ';\n') as kommentaarid
-      , group_concat( concat(k.isikukood, ': ', k.kirje) order by a.prioriteet desc SEPARATOR ';\n') as kirjed
+      , group_concat( DISTINCT
+          if(k.perenimi = '', NULL, UPPER(k.perenimi)) 
+          ORDER BY a.prioriteet DESC SEPARATOR ';')   AS perenimi
+      , group_concat( DISTINCT
+          if(k.eesnimi = '', NULL, REPLACE(UPPER(k.eesnimi),'ALEKSANDR','ALEKSANDER')) 
+          ORDER BY a.prioriteet DESC SEPARATOR ';')   AS eesnimi
+      , group_concat( DISTINCT
+          if(k.allikas = 'RK', NULL, if(k.isanimi = '', NULL, UPPER(k.isanimi))) 
+          ORDER BY a.prioriteet DESC SEPARATOR ';')   AS isanimi
+      , group_concat( DISTINCT
+          if(k.sünd = '', NULL, LEFT(k.sünd,4)) 
+          ORDER BY a.prioriteet DESC SEPARATOR ';')   AS sünd
+      , group_concat( DISTINCT
+          if(k.surm = '', NULL, LEFT(k.surm,4)) 
+          ORDER BY a.prioriteet DESC SEPARATOR ';')   AS surm
+      , group_concat( DISTINCT
+          if(k.kommentaar = '', NULL, k.kommentaar) 
+          ORDER BY a.prioriteet DESC SEPARATOR ';\n') AS kommentaarid
+      , group_concat(
+          concat(k.isikukood, ': ', k.kirje) 
+          ORDER BY a.prioriteet DESC SEPARATOR ';\n') AS kirjed
       from kirjed k
       left join v_prioriteedid a on a.kood = k.allikas
       where k.emi_id = _emi_id
@@ -50,7 +62,7 @@ BEGIN
     WHERE e.id = _emi_id;
 END;;
 
-CREATE or replace PROCEDURE `EMI_check_record`(IN _ik CHAR(10))
+CREATE or REPLACE PROCEDURE `EMI_check_record`(IN _ik CHAR(10))
 BEGIN
     SELECT emi_id INTO @emi_id FROM kirjed WHERE isikukood = _ik;
     
--- docs/sql/view_publish.sql
+++ docs/sql/view_publish.sql
@@ -1,10 +1,10 @@
 CREATE OR REPLACE VIEW v_publish AS
 SELECT id AS emi_id
-     , ifnull(em.EmiPerenimi, em.Perenimi) AS Perenimi
-     , ifnull(em.EmiEesnimi, em.Eesnimi) AS Eesnimi
-     , ifnull(em.EmiIsanimi, em.Isanimi) AS Isanimi
-     , ifnull(em.EmiSünd, em.Sünd) AS Sünd
-     , ifnull(em.EmiSurm, em.Surm) AS Surm
+     , ifnull(em.EmiPerenimi, left(em.Perenimi, locate(';', concat(em.Perenimi, ';'))-1)) AS Perenimi
+     , ifnull(em.EmiEesnimi, left(em.Eesnimi, locate(';', concat(em.Eesnimi, ';'))-1)) AS Eesnimi
+     , ifnull(em.EmiIsanimi, left(em.Isanimi, locate(';', concat(em.Isanimi, ';'))-1)) AS Isanimi
+     , left(ifnull(em.EmiSünd, left(em.Sünd, locate(';', concat(em.Sünd, ';'))-1)), 4) AS Sünd
+     , left(ifnull(em.EmiSurm, left(em.Surm, locate(';', concat(em.Surm, ';'))-1)), 4) AS Surm
      , replace(em.Kirjed, '"', "'") AS Kirjed
 FROM EMIR em
 WHERE id IN (SELECT DISTINCT emi_id FROM kirjed WHERE kivi = '!');
