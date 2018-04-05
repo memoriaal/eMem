c7ce368042a24b74026561958fe36857dc9bee81
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2018-04-05 16:57:28 +0300

    NK refresh

--- docs/sql/proc_NK_refresh.sql
+++ docs/sql/proc_NK_refresh.sql
@@ -1,16 +1,43 @@
 DELIMITER ;;
 CREATE OR REPLACE DEFINER=`queue`@`localhost` PROCEDURE `NK_refresh`(IN _emi_id INT(11) UNSIGNED)
-BEGIN
+proc_label:BEGIN
 
+  SET @ik = NULL;
+  
   SELECT isikukood INTO @ik
   FROM kirjed
   WHERE emi_id = _emi_id
   AND allikas = 'Nimekujud'
   LIMIT 1;
   
+  -- Kui pole NK kirjet
+  IF @ik IS NULL THEN
+    
+    SELECT COUNT(1) INTO @cnt FROM kirjed k
+    WHERE k.emi_id = _emi_id
+    AND k.MR != '!'
+    AND k.Puudulik != '!'
+    AND k.Allikas NOT IN (
+      SELECT Kood FROM allikad
+      WHERE nonPerson = 1
+    );
+    
+    IF @cnt = 0 THEN
+      LEAVE proc_label;
+    END IF;
+    
+    INSERT INTO kirjed (isikukood, emi_id, allikas)
+    SELECT lpad(max(right(isikukood, 7))+1, 10, 'NK-0000000') AS isikukood
+         , 1 as emi_id 
+         , 'Nimekujud' as allikas
+      FROM kirjed where allikas = 'Nimekujud';
+    
+  END IF;
+  
+  
   update kirjed k left join
   (
-        select nk.isikukood
+        SELECT nk.isikukood
         , SUBSTRING_INDEX(group_concat(
             if(k.perenimi = ''   OR a.prioriteetPerenimi = 0, '', UPPER(k.perenimi))
             ORDER BY a.prioriteetPerenimi DESC SEPARATOR ';'), ';', 1)
@@ -45,12 +72,12 @@ BEGIN
         and nk.isikukood = @ik
         group by k.emi_id
   ) as nimekuju on nimekuju.isikukood = k.isikukood
-  set k.perenimi = nimekuju.perenimi
-    , k.eesnimi = nimekuju.eesnimi
-    , k.isanimi = nimekuju.isanimi
-    , k.emanimi = nimekuju.emanimi
-    , k.sünd = nimekuju.sünd
-    , k.surm = nimekuju.surm
+  set k.perenimi = ifnull(nimekuju.perenimi, '')
+    , k.eesnimi = ifnull(nimekuju.eesnimi, '')
+    , k.isanimi = ifnull(nimekuju.isanimi, '')
+    , k.emanimi = ifnull(nimekuju.emanimi, '')
+    , k.sünd = ifnull(nimekuju.sünd, '')
+    , k.surm = ifnull(nimekuju.surm, '')
     , kirje = 
       concat_ws('. '
         , concat_ws(', '
