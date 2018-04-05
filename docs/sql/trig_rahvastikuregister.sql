bceaa3f6436a0b9af01d2a21fad12735a25c64c1
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2018-04-05 16:57:09 +0300

    fix

--- docs/sql/trig_rahvastikuregister.sql
+++ docs/sql/trig_rahvastikuregister.sql
@@ -2,7 +2,7 @@ DELIMITER ;;
 
 CREATE OR REPLACE TRIGGER rahvastikuregister_BU BEFORE UPDATE ON rahvastikuregister FOR EACH ROW
 BEGIN
-  IF NEW.seos is not null AND OLD.seos != new.seos THEN
+  IF NEW.seos IS NOT NULL AND (OLD.seos IS NULL OR OLD.seos != new.seos) THEN
     INSERT IGNORE INTO z_queue (isikukood1, isikukood2, task, user)
     VALUES (NEW.kirjekood, NEW.seos, 'Import from RR', user());
   END IF;
