9a6267e3be5f9b4be44958bf18e49f3276602645
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2018-04-05 14:49:06 +0300

    fix

--- docs/sql/proc_synchronize_checklist.sql
+++ docs/sql/proc_synchronize_checklist.sql
@@ -1,6 +1,6 @@
 DELIMITER ;;
 CREATE OR REPLACE DEFINER=`queue`@`localhost` PROCEDURE `synchronize_checklist`(IN ik1 CHAR(10), IN ik2 CHAR(10))
-BEGIN
+proc_label:BEGIN
     DECLARE kivi1, mittekivi1, rel1, mr1 enum('', '!');
     DECLARE kivi2, mittekivi2, rel2, mr2 enum('', '!');
 
@@ -10,9 +10,19 @@ BEGIN
       INTO kivi1, mittekivi1, rel1, mr1
       FROM kirjed WHERE isikukood = ik1;
 
+    IF @kivi1 IS NULL
+    THEN
+        LEAVE proc_label;
+    END IF;
+
     SELECT kivi, mittekivi, rel, mr
       INTO kivi2, mittekivi2, rel2, mr2
       FROM kirjed WHERE isikukood = ik2;
+
+    IF @kivi2 IS NULL
+    THEN
+        LEAVE proc_label;
+    END IF;
 
     IF kivi1 = '!' THEN
         SET @_kivi = kivi1;
