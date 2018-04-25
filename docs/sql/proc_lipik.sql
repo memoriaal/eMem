fc762453b2c2e2c41061c113e48cea36e51da382
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2018-04-25 12:47:40 +0300

    sildid-lipikud

--- docs/sql/proc_lipik.sql
+++ docs/sql/proc_lipik.sql
@@ -5,13 +5,48 @@ BEGIN
     SET kirjekood = _ik, lipik = _lipik, user = _user
     ON DUPLICATE KEY UPDATE kustutatud = mod(kustutatud+1,2), user = _user;
 
+    CALL lipik_refresh(_ik);
+END;;
+DELIMITER ;
+
+DELIMITER ;;
+CREATE OR REPLACE DEFINER=`queue`@`localhost` PROCEDURE lipik_refresh(IN _ik CHAR(10))
+BEGIN
     SET @lipikstr = NULL;
     SELECT GROUP_CONCAT(concat(lipik, ' - ', user, '@', now()) SEPARATOR '; \n') INTO @lipikstr
     FROM kirjelipikud
     WHERE kirjekood = _ik
     AND kustutatud = 0;
 
-    UPDATE kirjed SET lipikudCSV = @lipikstr, user = _user
+    UPDATE kirjed SET lipikudCSV = @lipikstr
     WHERE isikukood = _ik;
+END;;
+DELIMITER ;
+
+DELIMITER ;;
+CREATE OR REPLACE DEFINER=`queue`@`localhost` PROCEDURE lipik_refresh_all()
+BEGIN
+
+    DECLARE _ik CHAR(10);
+    DECLARE finished INTEGER DEFAULT 0;
+
+    DECLARE cur1 CURSOR FOR
+        SELECT kirjekood
+        FROM kirjelipikud
+        WHERE kustutatud = 0;
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET finished = 1;
+
+    OPEN cur1;
+    read_loop: LOOP
+        FETCH cur1 INTO _ik;
+        IF finished = 1 THEN
+            LEAVE read_loop;
+        END IF;
+
+        CALL lipik_refresh(_ik);
+
+    END LOOP;
+    CLOSE cur1;
+    SET finished = 0;
 END;;
 DELIMITER ;
--- docs/sql/proc_silt.sql
+++ docs/sql/proc_silt.sql
@@ -5,13 +5,48 @@ BEGIN
     SET kirjekood = _ik, silt = _silt, user = _user
     ON DUPLICATE KEY UPDATE kustutatud = mod(kustutatud+1,2), user = _user;
 
+    CALL silt_refresh(_ik, _user);
+END;;
+DELIMITER ;
+
+DELIMITER ;;
+CREATE OR REPLACE DEFINER=`queue`@`localhost` PROCEDURE silt_refresh(IN _ik CHAR(10))
+BEGIN
     SET @labelstr = NULL;
     SELECT GROUP_CONCAT(concat(silt, ' - ', user, '@', now()) SEPARATOR '; \n') INTO @labelstr
     FROM kirjesildid
     WHERE kirjekood = _ik
     AND kustutatud = 0;
 
-    UPDATE kirjed SET sildidCSV = @labelstr, user = _user
+    UPDATE kirjed SET sildidCSV = @labelstr
     WHERE isikukood = _ik;
+END;;
+DELIMITER ;
+
+DELIMITER ;;
+CREATE OR REPLACE DEFINER=`queue`@`localhost` PROCEDURE silt_refresh_all()
+BEGIN
+
+    DECLARE _ik CHAR(10);
+    DECLARE finished INTEGER DEFAULT 0;
+
+    DECLARE cur1 CURSOR FOR
+        SELECT kirjekood
+        FROM kirjesildid
+        WHERE kustutatud = 0;
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET finished = 1;
+
+    OPEN cur1;
+    read_loop: LOOP
+        FETCH cur1 INTO _ik;
+        IF finished = 1 THEN
+            LEAVE read_loop;
+        END IF;
+
+        CALL silt_refresh(_ik);
+
+    END LOOP;
+    CLOSE cur1;
+    SET finished = 0;
 END;;
 DELIMITER ;
