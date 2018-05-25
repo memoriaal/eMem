db75956c3e2949019b682d19c10d5f2035bfa2f6
michelek <mihkel.putrinsh@gmail.com>
2018-05-25 12:26:54 +0300

    topelt kivikirjed

--- docs/sql/repis/aruanded.sql
+++ docs/sql/repis/aruanded.sql
@@ -45,80 +45,77 @@ FROM ((repis.v_kirjesildid s
   where s.silt = 'x - kivi' and k.kirjekood is null and k1.Perenimi <> '' and k1.Eesnimi <> '';
 
 
-CREATE OR REPLACE VIEW topelt_kivikirjed
-AS SELECT DISTINCT
-   k2.persoon AS persoon,
-   k2.kirjekood AS kirjekood,
-   k2.Perenimi AS perenimi,
-   k2.Eesnimi AS eesnimi,
-   k2.Isanimi AS isanimi,
-   k2.Emanimi AS emanimi,
-   k2.Sünd AS sünd,
-   k2.Surm AS surm,
-   k2.kommentaar AS kommentaar
-FROM repis.kirjed k0
-LEFT JOIN repis.kirjed k1 ON k0.persoon = k1.persoon AND k0.kirjekood != k1.kirjekood AND k1.allikas = 'KIVI'
-LEFT JOIN repis.kirjed k2 ON k2.persoon = k1.persoon AND k2.allikas = 'KIVI'
-WHERE k0.allikas = 'KIVI'
-AND k1.persoon IS NOT NULL
-;
+  CREATE OR REPLACE VIEW `topelt_kivikirjed`
+  AS SELECT
+     k2.persoon AS persoon,
+     k2.kirjekood AS kirjekood,
+     k2.Kirje AS Kirje,
+     k2.Kommentaar AS Kommentaar,
+     k2.Perenimi AS Perenimi,
+     k2.Eesnimi AS Eesnimi,
+     k2.Isanimi AS Isanimi,
+     k2.Emanimi AS Emanimi,
+     k2.Sünd AS Sünd,
+     k2.Surm AS Surm,
+     k2.Perekood AS Perekood,
+     k2.Sugu AS Sugu,
+     k2.Rahvus AS Rahvus,
+     k2.Välisviide AS Välisviide,
+     k2.Allikas AS Allikas,
+     k2.Nimekiri AS Nimekiri,
+     k2.Puudulik AS Puudulik,
+     k2.EkslikKanne AS EkslikKanne,
+     k2.Peatatud AS Peatatud,
+     k2.kustuta AS kustuta,
+     k2.created_at AS created_at,
+     k2.created_by AS created_by,
+     k2.updated_at AS updated_at,
+     k2.updated_by AS updated_by
+  FROM repis.kirjed k2
+  right join repis.kirjed k1 on k2.persoon = k1.persoon
+                            and k2.kirjekood <> k1.kirjekood
+  right join repis.kirjed k0 on k1.persoon = k0.persoon
+  where k0.Allikas = 'KIVI'
+    and k1.Allikas = 'KIVI'
+    and k2.Allikas = 'KIVI'
+    and k1.persoon is not NULL
+  -- GROUP BY k1.kirjekood;
+  ;
 
-CREATE OR REPLACE VIEW topelt_kivikirjed
-AS SELECT
-   distinct `k2`.`persoon` AS `persoon`,
-   `k2`.`kirjekood` AS `kirjekood`,
-   `k2`.`Kirje` AS `Kirje`,
-   `k2`.`Perenimi` AS `Perenimi`,
-   `k2`.`Eesnimi` AS `Eesnimi`,
-   `k2`.`Isanimi` AS `Isanimi`,
-   `k2`.`Emanimi` AS `Emanimi`,
-   `k2`.`Sünd` AS `Sünd`,
-   `k2`.`Surm` AS `Surm`,
-   `k2`.`Perekood` AS `Perekood`,
-   `k2`.`Sugu` AS `Sugu`,
-   `k2`.`Rahvus` AS `Rahvus`,
-   `k2`.`Välisviide` AS `Välisviide`,
-   `k2`.`Kommentaar` AS `Kommentaar`,
-   `k2`.`Allikas` AS `Allikas`,
-   `k2`.`Nimekiri` AS `Nimekiri`,
-   `k2`.`Puudulik` AS `Puudulik`,
-   `k2`.`EkslikKanne` AS `EkslikKanne`,
-   `k2`.`Peatatud` AS `Peatatud`,
-   `k2`.`kustuta` AS `kustuta`,
-   `k2`.`created_at` AS `created_at`,
-   `k2`.`created_by` AS `created_by`,
-   `k2`.`updated_at` AS `updated_at`,
-   `k2`.`updated_by` AS `updated_by`
-FROM ((`repis`.`kirjed` `k2` join `repis`.`kirjed` `k1`) join `repis`.`kirjed` `k0`) where `k0`.`persoon` = `k1`.`persoon` and `k0`.`kirjekood` <> `k1`.`kirjekood` and `k1`.`Allikas` = 'KIVI' and `k2`.`persoon` = `k1`.`persoon` and `k2`.`Allikas` = 'KIVI' and `k0`.`Allikas` = 'KIVI' and `k1`.`persoon` is not null;
+  CREATE OR REPLACE VIEW `topelt_kivikirjed_b`
+  AS SELECT
+     k.persoon AS persoon,
+     k.kirjekood AS kirjekood,
+     k.Kirje AS Kirje,
+     k.Kommentaar AS Kommentaar,
+     k.Perenimi AS Perenimi,
+     k.Eesnimi AS Eesnimi,
+     k.Isanimi AS Isanimi,
+     k.Emanimi AS Emanimi,
+     k.Sünd AS Sünd,
+     k.Surm AS Surm,
+     k.Perekood AS Perekood,
+     k.Sugu AS Sugu,
+     k.Rahvus AS Rahvus,
+     k.Välisviide AS Välisviide,
+     k.Allikas AS Allikas,
+     k.Nimekiri AS Nimekiri,
+     k.Puudulik AS Puudulik,
+     k.EkslikKanne AS EkslikKanne,
+     k.Peatatud AS Peatatud,
+     k.kustuta AS kustuta,
+     k.created_at AS created_at,
+     k.created_by AS created_by,
+     k.updated_at AS updated_at,
+     k.updated_by AS updated_by
+  FROM repis.kirjed k1
+  LEFT JOIN repis.kirjed k2 ON k2.persoon = k1.persoon AND k2.allikas = 'kivi' AND k2.kirjekood != k1.kirjekood
+  LEFT JOIN repis.kirjed k ON k.persoon = k2.persoon
+  WHERE k1.allikas = 'kivi'
+  AND k2.persoon IS NOT NULL
+  ;
 
 
-CREATE OR REPLACE VIEW topelt_kivikirjed AS
-SELECT DISTINCT
-   k2.persoon AS persoon,
-   k2.kirjekood AS kirjekood,
-   k2.Kirje AS Kirje,
-   k2.Kommentaar AS Kommentaar,
-   k2.Perenimi AS Perenimi,
-   k2.Eesnimi AS Eesnimi,
-   k2.Isanimi AS Isanimi,
-   k2.Emanimi AS Emanimi,
-   k2.Sünd AS Sünd,
-   k2.Surm AS Surm,
-   k2.Perekood AS Perekood,
-   k2.Sugu AS Sugu,
-   k2.Rahvus AS Rahvus,
-   k2.Välisviide AS Välisviide,
-   k2.Allikas AS Allikas,
-   k2.Nimekiri AS Nimekiri,
-   k2.Puudulik AS Puudulik,
-   k2.EkslikKanne AS EkslikKanne,
-   k2.Peatatud AS Peatatud,
-   k2.kustuta AS kustuta,
-   k2.created_at AS created_at,
-   k2.created_by AS created_by,
-   k2.updated_at AS updated_at,
-   k2.updated_by AS updated_by
-FROM ((`repis`.`kirjed` `k2` join `repis`.`kirjed` `k1`) join `repis`.`kirjed` `k0`) where `k0`.`persoon` = `k1`.`persoon` and `k0`.`kirjekood` <> `k1`.`kirjekood` and `k1`.`Allikas` = 'KIVI' and `k2`.`persoon` = `k1`.`persoon` and `k2`.`Allikas` = 'KIVI' and `k0`.`Allikas` = 'KIVI' and `k1`.`persoon` is not null;
 
 
 --            __  __                                     _                       _
