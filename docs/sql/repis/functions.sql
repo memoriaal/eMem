08aaa816985b5ea70dcf3219de9f47a16a0f3559
michelek <mihkel.putrinsh@gmail.com>
2018-06-04 16:58:08 +0300

    sildid, lipikud @ desktop

--- docs/sql/repis/functions.sql
+++ docs/sql/repis/functions.sql
@@ -1,35 +1,115 @@
 
-DELIMITER ;; -- func_kirje_lipikud
+DELIMITER ;; -- func_kirjelipikud()
 
-  CREATE OR REPLACE DEFINER=queue@localhost FUNCTION repis.func_kirje_lipikud(
-        _kirjekood CHAR(10)
-    ) RETURNS VARCHAR(2000) CHARSET utf8 COLLATE utf8_estonian_ci
+  CREATE OR REPLACE DEFINER=queue@localhost FUNCTION repis.func_kirjelipikud(
+      _kirjekood CHAR(10)
+  ) RETURNS TEXT CHARSET utf8 COLLATE  utf8_estonian_ci
+  func_label:BEGIN
 
-    func_label:BEGIN
+    SET @omad_lipikud = NULL, @teiste_lipikud = NULL;
 
-      SELECT persoon INTO @persoon FROM repis.kirjed WHERE kirjekood = _kirjekood;
+    SELECT GROUP_CONCAT(kl.lipik SEPARATOR '; ') INTO @omad_lipikud
+    FROM repis.v_kirjelipikud kl
+    RIGHT JOIN repis.kirjed k ON k.kirjekood = kl.kirjekood
+    RIGHT JOIN repis.kirjed k0 ON k0.persoon = k.persoon
+    WHERE k0.kirjekood = _kirjekood
+    AND kl.deleted_at = '0000-00-00 00:00:00'
+    AND kl.kirjekood = k0.kirjekood
+    GROUP BY kl.kirjekood;
 
-      SELECT group_concat(v.lipik, ' [', v.created_by , '@', v.created_at, ']' SEPARATOR ' \n') INTO @pre
-      FROM repis.v_kirjelipikud v
-      WHERE v.kirjekood = @kirjekood
-      AND v.deleted_at = '0000-00-00 00:00:00'
-      ;
+    SELECT GROUP_CONCAT(concat_ws(':', k.kirjekood, kl.lipik) SEPARATOR '; ') INTO @teiste_lipikud
+    FROM repis.v_kirjelipikud kl
+    RIGHT JOIN repis.kirjed k ON k.kirjekood = kl.kirjekood
+    RIGHT JOIN repis.kirjed k0 ON k0.persoon = k.persoon
+    WHERE k0.kirjekood = _kirjekood
+    AND kl.deleted_at = '0000-00-00 00:00:00'
+    AND kl.kirjekood != k0.kirjekood
+    GROUP BY k0.persoon;
 
-      SELECT group_concat(v.kirjekood, ':', v.lipik, ' [', v.created_by , '@', v.created_at, ']' SEPARATOR ' \n') INTO @post
-      FROM repis.v_kirjelipikud v
-      RIGHT JOIN repis.kirjed k ON k.kirjekood = v.kirjekood
-      WHERE k.persoon = @persoon
-      AND v.kirjekood IS NOT NULL
-      AND k.kirjekood != @kirjekood
-      AND v.deleted_at = '0000-00-00 00:00:00'
-      ;
+    RETURN concat_ws('\n', @omad_lipikud, @teiste_lipikud);
 
-      RETURN concat_ws('',
-        concat(@pre, '\n'),
-        '---',
-        concat('\n', @post)
-      ) COLLATE utf8_estonian_ci;
+  END;;
 
-    END;;
+DELIMITER ;
 
+
+DELIMITER ;; -- func_kirjesildid()
+
+  CREATE OR REPLACE DEFINER=queue@localhost FUNCTION repis.func_kirjesildid(
+      _kirjekood CHAR(10)
+  ) RETURNS TEXT CHARSET utf8 COLLATE  utf8_estonian_ci
+  func_label:BEGIN
+
+    SET @omad_sildid = NULL, @teiste_sildid = NULL;
+
+    SELECT GROUP_CONCAT(ks.silt SEPARATOR '; ') INTO @omad_sildid
+    FROM repis.v_kirjesildid ks
+    RIGHT JOIN repis.kirjed k ON k.kirjekood = ks.kirjekood
+    RIGHT JOIN repis.kirjed k0 ON k0.persoon = k.persoon
+    WHERE k0.kirjekood = _kirjekood
+    AND ks.deleted_at = '0000-00-00 00:00:00'
+    AND ks.kirjekood = k0.kirjekood
+    GROUP BY ks.kirjekood;
+
+    SELECT GROUP_CONCAT(concat_ws(':', k.kirjekood, ks.silt) SEPARATOR '; ') INTO @teiste_sildid
+    FROM repis.v_kirjesildid ks
+    RIGHT JOIN repis.kirjed k ON k.kirjekood = ks.kirjekood
+    RIGHT JOIN repis.kirjed k0 ON k0.persoon = k.persoon
+    WHERE k0.kirjekood = _kirjekood
+    AND ks.deleted_at = '0000-00-00 00:00:00'
+    AND ks.kirjekood != k0.kirjekood
+    GROUP BY k0.persoon;
+
+    RETURN concat_ws('\n', @omad_sildid, @teiste_sildid);
+
+  END;;
+
+DELIMITER ;
+
+
+
+
+DELIMITER ;;  -- unrepeat
+CREATE OR REPLACE FUNCTION repis.func_unrepeat(
+    _word VARCHAR(100),
+    _level ENUM('soft', 'hard')
+) RETURNS VARCHAR(100) CHARSET utf8 COLLATE utf8_estonian_ci
+BEGIN
+
+    DECLARE i INTEGER;
+    DECLARE _out VARCHAR(100);
+    SET i = 0;
+    SET _out = '';
+
+    SET _word = REPLACE(_word, 'TH', 'T');
+    SET _word = REPLACE(_word, 'SH', 'Š');
+    SET _word = REPLACE(_word, 'CH', 'Š');
+    SET _word = REPLACE(_word, 'ZH', 'Z');
+    SET _word = REPLACE(_word, 'TZ', 'Z');
+    IF _level = 'hard' THEN
+      SET _word = REPLACE(_word, 'S' , 'Š');
+      SET _word = REPLACE(_word, 'Z' , 'Š');
+      SET _word = REPLACE(_word, 'Ž' , 'Š');
+      SET _word = REPLACE(_word, 'C' , 'Š');
+      SET _word = REPLACE(_word, 'A', 'Ẵ');
+      SET _word = REPLACE(_word, 'E', 'Ẵ');
+      SET _word = REPLACE(_word, 'I', 'Ẵ');
+      SET _word = REPLACE(_word, 'O', 'Ẵ');
+      SET _word = REPLACE(_word, 'U', 'Ẵ');
+      SET _word = REPLACE(_word, 'Õ', 'Ẵ');
+      SET _word = REPLACE(_word, 'Ä', 'Ẵ');
+      SET _word = REPLACE(_word, 'Ö', 'Ẵ');
+      SET _word = REPLACE(_word, 'Ü', 'Ẵ');
+    END IF;
+
+    myloop: WHILE (i <= LENGTH(_word)) DO
+        IF SUBSTRING(_word, i, 1) != SUBSTRING(_word, i+1, 1) THEN
+          SET _out = concat(_out, SUBSTRING(_word, i, 1));
+        END IF;
+        SET i = i + 1;
+    END WHILE;
+
+    RETURN(_out);
+
+END;;
 DELIMITER ;
