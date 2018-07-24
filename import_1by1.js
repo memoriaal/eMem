310c98c1fe79d1bc5b1b47e6abe0888a2971dc13
michelek <mihkel.putrinsh@gmail.com>
2018-07-24 15:45:58 +0300

    import scripts

--- import_1by1.js
+++ import_1by1.js
@@ -8,9 +8,9 @@ var first_line_is_for_labels = true
 const ES_CREDENTIALS = process.env.ES_CREDENTIALS
 const INDEX = process.env.INDEX
 const SOURCE = process.env.SOURCE
-const QUEUE_LENGTH = 1
-const BULK_SIZE = 507
-// const BULK_SIZE = 4100
+const QUEUE_LENGTH = 2
+// const BULK_SIZE = 507
+const BULK_SIZE = 4100
 const START_TIME = Date.now()
 
 
