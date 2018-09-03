1153f04d01c0c7b42a65860741ef028ec9c3b646
michelek <mihkel.putrinsh@gmail.com>
2018-09-03 15:30:41 +0300

    autoexport

--- update_memoriaal_ee.sh
+++ update_memoriaal_ee.sh
@@ -3,15 +3,16 @@
 . ~/credentials.txt
 
 csv_filename="/paringud/$(date +%Y%m%d_%H%M%S)_memoriaal_ee.csv"
+
 mysql -u"${M_MYSQL_U}" -p"${M_MYSQL_P}" aruanded<<EOFMYSQL
-SELECT   *
-FROM memoriaal_ee
+SELECT * from aruanded.memoriaal_ee
 INTO OUTFILE '${csv_filename}'
+CHARACTER SET utf8
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n';
 EOFMYSQL
 
-INDEX=persons1 SOURCE='${csv_filename}' ES_CREDENTIALS='${ELASTIC_C}' node ~/scripts/import_1by1.js
+INDEX=persons SOURCE=${csv_filename} ES_CREDENTIALS='${ELASTIC_C}' node ~/scripts/import_once.js
 
-rm '${csv_filename}'
+# rm ${csv_filename}
