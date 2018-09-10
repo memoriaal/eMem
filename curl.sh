5983797686167934e89bede2fc6a9d064770bc6d
michelek <mihkel.putrinsh@gmail.com>
2018-09-10 16:13:02 +0300

    curls

--- curl.sh
+++ curl.sh
@@ -1,5 +1,5 @@
 # add some fielddata mapping
-curl -X PUT "https://elastic:xxxxxxx@94abc9318c712977e8c684628aa5ea0f.us-east-1.aws.found.io:9243/persons/_mapping/isik" -H 'Content-Type: application/json' -d'
+curl -X PUT "https://elastic:XXX@94abc9318c712977e8c684628aa5ea0f.us-east-1.aws.found.io:9243/allpersons/_mapping/isik" -H 'Content-Type: application/json' -d'
 {
   "properties": {
     "eesnimi": {
@@ -14,13 +14,15 @@ curl -X PUT "https://elastic:xxxxxxx@94abc9318c712977e8c684628aa5ea0f.us-east-1.
 }
 '
 
-curl -X POST "https://Delfi:B68-xvu-gds-HA8@94abc9318c712977e8c684628aa5ea0f.us-east-1.aws.found.io:9243/persons/_search?size=2&scroll=1m&pretty" -H 'Content-Type: application/json' -d' {
+curl -X POST "https://Delfi:B68-xvu-gds-HA8@94abc9318c712977e8c684628aa5ea0f.us-east-1.aws.found.io:9243/allpersons/_search?size=2&scroll=1m&pretty" -H 'Content-Type: application/json' -d' {
   "query": {
     "match": { "kivi": "1" }
   },
   "sort": [
+    {"perenimi": "asc"},
     {"eesnimi": "asc"}
-  ]
+  ],
+  "_source": [ "id", "perenimi", "eesnimi", "isanimi", "emanimi", "sünd", "surm" ],
 }'
 
 curl -X POST "https://Delfi:B68-xvu-gds-HA8@94abc9318c712977e8c684628aa5ea0f.us-east-1.aws.found.io:9243/_search/scroll?pretty" -H 'Content-Type: application/json' -d' {"scroll":"1m", "scroll_id":"DnF1ZXJ5VGhlbkZldGNoBQAAAAAABjbmFk1CZ2JjT2lUUzNDWTVOQmdwVFdWTmcAAAAAAAY25RZNQmdiY09pVFMzQ1k1TkJncFRXVk5nAAAAAAAGNugWTUJnYmNPaVRTM0NZNU5CZ3BUV1ZOZwAAAAAABjbnFk1CZ2JjT2lUUzNDWTVOQmdwVFdWTmcAAAAAAAY26RZNQmdiY09pVFMzQ1k1TkJncFRXVk5n"}'
@@ -28,8 +30,81 @@ curl -X POST "https://Delfi:B68-xvu-gds-HA8@94abc9318c712977e8c684628aa5ea0f.us-
 
 
 
-curl -X POST "https://Delfi:B68-xvu-gds-HA8@94abc9318c712977e8c684628aa5ea0f.us-east-1.aws.found.io:9243/allpersons/_search?size=2&scroll=1m&pretty" -H 'Content-Type: application/json' -d' {
+
+
+
+# add some fielddata mapping
+curl -X PUT "https://elastic:XXX@94abc9318c712977e8c684628aa5ea0f.us-east-1.aws.found.io:9243/allpersons/_mapping/isik" -H 'Content-Type: application/json' -d'
+{
+  "properties": {
+    "eesnimi": {
+      "type": "keyword"
+    },
+    "perenimi": {
+      "type": "keyword"
+    }
+  }
+}
+'
+
+curl -X POST "https://Delfi:B68-xvu-gds-HA8@94abc9318c712977e8c684628aa5ea0f.us-east-1.aws.found.io:9243/allpersons/_search?pretty" -H 'Content-Type: application/json' -d' {
   "query": {
-    "match": { "id": "0000073740" }
+    "multi_match": {
+      "query": "valdas",
+      "operator": "and",
+      "fields": [ "perenimi", "eesnimi" ],
+      "type": "cross_fields"
+    }
+  },
+  "sort": [
+    {"perenimi": "asc"},
+    {"eesnimi": "asc"}
+  ]
+}'
+
+curl -X POST "https://Delfi:B68-xvu-gds-HA8@94abc9318c712977e8c684628aa5ea0f.us-east-1.aws.found.io:9243/allpersons/_search?pretty" -H 'Content-Type: application/json' -d' {
+  "query": {
+    "multi_match": {
+      "query": "valdas",
+      "operator": "and",
+      "fields": [ "perenimi", "eesnimi" ],
+      "type": "cross_fields"
+    }
   }
 }'
+
+
+curl -X POST "https://Delfi:B68-xvu-gds-HA8@94abc9318c712977e8c684628aa5ea0f.us-east-1.aws.found.io:9243/allpersons/_search?pretty" -H 'Content-Type: application/json' -d' {
+  "query": {
+    "match": { "kivi": "1" }
+  },
+  "sort": [
+    {"perenimi": "asc"},
+    {"eesnimi": "asc"}
+  ],
+  "_source": [ "id", "perenimi", "eesnimi", "isanimi", "emanimi", "sünd", "surm" ]
+}'
+
+
+curl -X POST "https://Delfi:B68-xvu-gds-HA8@94abc9318c712977e8c684628aa5ea0f.us-east-1.aws.found.io:9243/allpersons/_search?pretty" -H 'Content-Type: application/json' -d' {
+  "query": {
+    "multi_match": {
+      "query": "valdas hans",
+      "operator": "and",
+      "fields": [ "_all" ],
+      "type": "cross_fields"
+    }
+  },
+  "sort": [
+    {"perenimi": "asc"},
+    {"eesnimi": "asc"}
+  ],
+  "_source": [ "id", "perenimi", "eesnimi", "isanimi", "emanimi", "sünd", "surm" ]
+}'
+
+
+# List all indices
+curl -X GET "https://elastic:XXX@94abc9318c712977e8c684628aa5ea0f.us-east-1.aws.found.io:9243/_cat/indices?v"
+
+# Delete index
+curl -X DELETE "elastic:XXX@94abc9318c712977e8c684628aa5ea0f.us-east-1.aws.found.io:9243/personsall"
