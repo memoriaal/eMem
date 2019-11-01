6c5cc4b887a56fd5a30c7368b6df23325ca6932c
michelek <mihkel.putrinsh@gmail.com>
2019-11-01 15:24:31 +0200

    Update aruanded.sql

--- docs/sql/repis/aruanded.sql
+++ docs/sql/repis/aruanded.sql
@@ -183,127 +183,137 @@ ORDER BY kk.perenimi, kk.eesnimi
 --    TS__[O]|_|__|_|  \___|  |_|_|_|  \___/   _|_|_   _|_|_  \__,_|  \__,_|   _|_|_
 --   {======|_|"""""|_|"""""|_|"""""|_|"""""|_|"""""|_|"""""|_|"""""|_|"""""|_|"""""|
 --  ./o--000'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'
+DELIMITER ;;
 
-CREATE OR REPLACE TABLE aruanded.memoriaal_ee (
-  id CHAR(10) COLLATE utf8_estonian_ci NOT NULL DEFAULT '',
-  perenimi VARCHAR(300) COLLATE utf8_estonian_ci NOT NULL DEFAULT '',
-  eesnimi VARCHAR(300) COLLATE utf8_estonian_ci NOT NULL DEFAULT '',
-  isanimi VARCHAR(300) COLLATE utf8_estonian_ci NOT NULL DEFAULT '',
-  emanimi VARCHAR(300) COLLATE utf8_estonian_ci NOT NULL DEFAULT '',
-  perenimed VARCHAR(300) COLLATE utf8_estonian_ci NOT NULL DEFAULT '',
-  eesnimed VARCHAR(300) COLLATE utf8_estonian_ci NOT NULL DEFAULT '',
-  isanimed VARCHAR(300) COLLATE utf8_estonian_ci NOT NULL DEFAULT '',
-  emanimed VARCHAR(300) COLLATE utf8_estonian_ci NOT NULL DEFAULT '',
-  sünd VARCHAR(300) COLLATE utf8_estonian_ci NOT NULL DEFAULT '',
-  surm VARCHAR(300) COLLATE utf8_estonian_ci NOT NULL DEFAULT '',
-  kivi VARCHAR(1) COLLATE utf8_estonian_ci NOT NULL DEFAULT '',
+  CREATE OR REPLACE PROCEDURE aruanded.memoriaal_ee()
+  proc_label:BEGIN
 
-  tahvlikirje varchar(43) COLLATE utf8_estonian_ci NOT NULL DEFAULT '',
-  tiib VARCHAR(1) COLLATE utf8_estonian_ci NOT NULL DEFAULT '',
-  tahvel VARCHAR(5) COLLATE utf8_estonian_ci NOT NULL DEFAULT '',
-  tulp VARCHAR(1) COLLATE utf8_estonian_ci NOT NULL DEFAULT '',
-  rida VARCHAR(2) COLLATE utf8_estonian_ci NOT NULL DEFAULT '',
+    CREATE OR REPLACE TABLE aruanded.memoriaal_ee (
+      id CHAR(10) COLLATE utf8_estonian_ci NOT NULL DEFAULT '',
+      perenimi VARCHAR(300) COLLATE utf8_estonian_ci NOT NULL DEFAULT '',
+      eesnimi VARCHAR(300) COLLATE utf8_estonian_ci NOT NULL DEFAULT '',
+      isanimi VARCHAR(300) COLLATE utf8_estonian_ci NOT NULL DEFAULT '',
+      emanimi VARCHAR(300) COLLATE utf8_estonian_ci NOT NULL DEFAULT '',
+      perenimed VARCHAR(300) COLLATE utf8_estonian_ci NOT NULL DEFAULT '',
+      eesnimed VARCHAR(300) COLLATE utf8_estonian_ci NOT NULL DEFAULT '',
+      isanimed VARCHAR(300) COLLATE utf8_estonian_ci NOT NULL DEFAULT '',
+      emanimed VARCHAR(300) COLLATE utf8_estonian_ci NOT NULL DEFAULT '',
+      sünd VARCHAR(300) COLLATE utf8_estonian_ci NOT NULL DEFAULT '',
+      surm VARCHAR(300) COLLATE utf8_estonian_ci NOT NULL DEFAULT '',
+      kivi VARCHAR(1) COLLATE utf8_estonian_ci NOT NULL DEFAULT '',
 
-  kirjed LONGTEXT COLLATE utf8_estonian_ci NOT NULL DEFAULT '',
-  pereseos LONGTEXT COLLATE utf8_estonian_ci NOT NULL DEFAULT '',
-  pereseosID TEXT COLLATE utf8_estonian_ci NOT NULL DEFAULT '',
-  relevantne tinyint(1) unsigned NOT NULL DEFAULT 0,
-  PRIMARY KEY (id)
-) ENGINE=INNODB DEFAULT CHARSET=utf8 COLLATE=utf8_estonian_ci AS
--- ;
-SELECT   nk.kirjekood AS id,
-         nk.perenimi,
-         nk.eesnimi,
-         nk.isanimi,
-         nk.emanimi,
-         replace(replace(group_concat(DISTINCT k.perenimi SEPARATOR ' '), ';', ' '), '-', ' ') AS perenimed,
-         replace(replace(group_concat(DISTINCT k.eesnimi SEPARATOR ' '), ';', ' '), '-', ' ') AS eesnimed,
-         replace(replace(group_concat(DISTINCT k.emanimi SEPARATOR ' '), ';', ' '), '-', ' ') AS emanimed,
-         replace(replace(group_concat(DISTINCT k.isanimi SEPARATOR ' '), ';', ' '), '-', ' ') AS isanimed,
-         LEFT(nk.sünd,4) AS sünd,
-         LEFT(nk.surm,4) AS surm,
-         IF(ks_k.silt IS NULL, '0', '1') AS kivi,
-         IF(ks_k.silt = 'x - kivi', IFNULL(kt.kirje, 'N/A'), '') AS tahvlikirje,
-         IF(ks_k.silt = 'x - kivi', IFNULL(kt.tiib, 'X'), '') AS tiib,
-         IF(ks_k.silt = 'x - kivi', IFNULL(kt.tahvel, 'X'), '') AS tahvel,
-         IF(ks_k.silt = 'x - kivi', IFNULL(kt.tulp, '-'), '') AS tulp,
-         IF(ks_k.silt = 'x - kivi', IFNULL(kt.rida, '-'), '') AS rida,
-         IF(evo.persoon IS NULL, '0', '1') AS ohvitser,
-         IFNULL(evo.Auaste, '') AS auaste,
-         IFNULL(evo.VR, '') AS VR,
-         IFNULL(evo.Nimi, '') AS evonimi,
-         IFNULL(evo.Kirje, '') AS evokirje,
-         concat(evo.sünd, '-', evo.surm) AS evoaastad,
-         IFNULL(REPLACE (
-           group_concat(DISTINCT
-             IF(
-               a.prioriteetkirje = 0, NULL, concat_ws('#|',
-                 k.persoon,
-                 k.kirjekood,
-                 IF (k.allikas = 'RR', REGEXP_REPLACE(k.kirje, '\\|+', '|'), k.kirje),
-                 a.allikas,
-                 a.nimetus,
-                 concat('{ "labels": ["',concat_ws('", "',
-                   IF(k.EiArvesta = '!', 'skip', NULL),
-                   IF(k.EkslikKanne = '!', 'wrong', NULL)
-                 ) , '"] }')
-               )
-             )
-             ORDER BY a.prioriteetkirje DESC SEPARATOR ';_\n'
-           ),
-           '"',
-           '\''
-         ), '')           AS kirjed,
-         IFNULL(REPLACE(
-           group_concat( DISTINCT
-              IF(
-                 kp.kirjekood IS NULL, NULL, concat_ws('#|',
-                 -- kp.raamatupere,
-                 kp.persoon,
-                 kp.kirjekood,
-                 kp.kirje,
-                 kpa.allikas,
-                 kpa.nimetus,
-                 concat('{ "labels": ["',concat_ws('", "',
-                   IF(kp.EiArvesta = '!', 'skip', NULL),
-                   IF(kp.EkslikKanne = '!', 'wrong', NULL)
-                 ) , '"] }')
-               )
-             )
-             ORDER BY kp.kirjekood ASC SEPARATOR ';_\n'
-           ),
-          '"',
-          '\''
-        ), '')           AS pereseos,
-        IFNULL(
-          group_concat( DISTINCT
-             IF( kp.kirjekood IS NULL, NULL, kp.persoon )
-             SEPARATOR ' '
-        ), '')           AS pereseosID,
-        IF( ks_mr.kirjekood IS NULL, 1, 0) as relevantne
-FROM repis.kirjed AS k
-LEFT JOIN repis.allikad AS a ON a.kood = k.allikas
-LEFT JOIN repis.kirjed AS kp ON kp.RaamatuPere <> '' AND kp.RaamatuPere = k.RaamatuPere AND kp.allikas != 'Persoon'
-LEFT JOIN repis.allikad AS kpa ON kpa.kood = kp.allikas
-LEFT JOIN repis.kirjed AS nk ON nk.persoon = k.persoon AND nk.allikas = 'Persoon' AND nk.persoon = nk.kirjekood
-LEFT JOIN repis.v_kirjesildid AS ks_k ON ks_k.kirjekood = nk.persoon AND ks_k.silt = 'x - kivi' AND ks_k.deleted_at = '0000-00-00 00:00:00'
-LEFT JOIN repis.v_kirjesildid AS ks_mr ON ks_mr.kirjekood = nk.persoon AND ks_mr.silt = 'x - mitterelevantne' AND ks_mr.deleted_at = '0000-00-00 00:00:00'
-LEFT JOIN import.memoriaal_kivitahvlid kt ON kt.persoon = k.persoon
-LEFT JOIN import.memoriaal_evo evo ON evo.persoon = k.persoon
-WHERE k.ekslikkanne = ''
-AND k.puudulik = ''
-AND k.allikas NOT IN ('KIVI')
--- AND k.persoon = '0000083261'
-AND k.peatatud = ''
--- AND kp.allikas != 'Persoon'
-AND nk.persoon IS NOT NULL
--- AND ks_mr.kirjekood IS NULL
-AND IFNULL(a.nonPerson, '') != '!'
-GROUP BY k.persoon
-HAVING perenimi != ''
-   AND kirjed != ''
--- ;
-INTO OUTFILE '/home/michelek/scripts/memoriaal_ee.csv'
-FIELDS TERMINATED BY ','
-ENCLOSED BY '"'
-LINES TERMINATED BY '\n';
+      tahvlikirje varchar(43) COLLATE utf8_estonian_ci NOT NULL DEFAULT '',
+      tiib VARCHAR(1) COLLATE utf8_estonian_ci NOT NULL DEFAULT '',
+      tahvel VARCHAR(5) COLLATE utf8_estonian_ci NOT NULL DEFAULT '',
+      tulp VARCHAR(1) COLLATE utf8_estonian_ci NOT NULL DEFAULT '',
+      rida VARCHAR(2) COLLATE utf8_estonian_ci NOT NULL DEFAULT '',
+
+      kirjed LONGTEXT COLLATE utf8_estonian_ci NOT NULL DEFAULT '',
+      pereseos LONGTEXT COLLATE utf8_estonian_ci NOT NULL DEFAULT '',
+      pereseosID TEXT COLLATE utf8_estonian_ci NOT NULL DEFAULT '',
+      relevantne tinyint(1) unsigned NOT NULL DEFAULT 0,
+      PRIMARY KEY (id)
+    ) ENGINE=INNODB DEFAULT CHARSET=utf8 COLLATE=utf8_estonian_ci AS
+    -- ;
+    SELECT   nk.kirjekood AS id,
+             nk.perenimi,
+             nk.eesnimi,
+             nk.isanimi,
+             nk.emanimi,
+             replace(replace(group_concat(DISTINCT k.perenimi SEPARATOR ' '), ';', ' '), '-', ' ') AS perenimed,
+             replace(replace(group_concat(DISTINCT k.eesnimi SEPARATOR ' '), ';', ' '), '-', ' ') AS eesnimed,
+             replace(replace(group_concat(DISTINCT k.emanimi SEPARATOR ' '), ';', ' '), '-', ' ') AS emanimed,
+             replace(replace(group_concat(DISTINCT k.isanimi SEPARATOR ' '), ';', ' '), '-', ' ') AS isanimed,
+             LEFT(nk.sünd,4) AS sünd,
+             LEFT(nk.surm,4) AS surm,
+             IF(ks_k.silt IS NULL, '0', '1') AS kivi,
+             IF(ks_k.silt = 'x - kivi', IFNULL(kt.kirje, 'N/A'), '') AS tahvlikirje,
+             IF(ks_k.silt = 'x - kivi', IFNULL(kt.tiib, 'X'), '') AS tiib,
+             IF(ks_k.silt = 'x - kivi', IFNULL(kt.tahvel, 'X'), '') AS tahvel,
+             IF(ks_k.silt = 'x - kivi', IFNULL(kt.tulp, '-'), '') AS tulp,
+             IF(ks_k.silt = 'x - kivi', IFNULL(kt.rida, '-'), '') AS rida,
+             IF(evo.persoon IS NULL, '0', '1') AS ohvitser,
+             IFNULL(evo.Auaste, '') AS auaste,
+             IFNULL(evo.VR, '') AS VR,
+             IFNULL(evo.Nimi, '') AS evonimi,
+             IFNULL(evo.Kirje, '') AS evokirje,
+             concat(evo.sünd, '-', evo.surm) AS evoaastad,
+             IFNULL(REPLACE (
+               group_concat(DISTINCT
+                 IF(
+                   a.prioriteetkirje = 0, NULL, concat_ws('#|',
+                     k.persoon,
+                     k.kirjekood,
+                     IF (k.allikas = 'RR', REGEXP_REPLACE(k.kirje, '\\|+', '|'), k.kirje),
+                     a.allikas,
+                     a.nimetus,
+                     concat('{ "labels": ["',concat_ws('", "',
+                       IF(k.EiArvesta = '!', 'skip', NULL),
+                       IF(k.EkslikKanne = '!', 'wrong', NULL)
+                     ) , '"] }')
+                   )
+                 )
+                 ORDER BY a.prioriteetkirje DESC SEPARATOR ';_\n'
+               ),
+               '"',
+               '\''
+             ), '')           AS kirjed,
+             IFNULL(REPLACE(
+               group_concat( DISTINCT
+                  IF(
+                     kp.kirjekood IS NULL, NULL, concat_ws('#|',
+                     -- kp.raamatupere,
+                     kp.persoon,
+                     kp.kirjekood,
+                     kp.kirje,
+                     kpa.allikas,
+                     kpa.nimetus,
+                     concat('{ "labels": ["',concat_ws('", "',
+                       IF(kp.EiArvesta = '!', 'skip', NULL),
+                       IF(kp.EkslikKanne = '!', 'wrong', NULL)
+                     ) , '"] }')
+                   )
+                 )
+                 ORDER BY kp.kirjekood ASC SEPARATOR ';_\n'
+               ),
+              '"',
+              '\''
+            ), '')           AS pereseos,
+            IFNULL(
+              group_concat( DISTINCT
+                 IF( kp.kirjekood IS NULL, NULL, kp.persoon )
+                 SEPARATOR ' '
+            ), '')           AS pereseosID,
+            IF( ks_mr.kirjekood IS NULL, 1, 0) as relevantne
+    FROM repis.kirjed AS k
+    LEFT JOIN repis.allikad AS a ON a.kood = k.allikas
+    LEFT JOIN repis.kirjed AS kp ON kp.RaamatuPere <> '' AND kp.RaamatuPere = k.RaamatuPere AND kp.allikas != 'Persoon'
+    LEFT JOIN repis.allikad AS kpa ON kpa.kood = kp.allikas
+    LEFT JOIN repis.kirjed AS nk ON nk.persoon = k.persoon AND nk.allikas = 'Persoon' AND nk.persoon = nk.kirjekood
+    LEFT JOIN repis.v_kirjesildid AS ks_k ON ks_k.kirjekood = nk.persoon AND ks_k.silt = 'x - kivi' AND ks_k.deleted_at = '0000-00-00 00:00:00'
+    LEFT JOIN repis.v_kirjesildid AS ks_mr ON ks_mr.kirjekood = nk.persoon AND ks_mr.silt = 'x - mitterelevantne' AND ks_mr.deleted_at = '0000-00-00 00:00:00'
+    LEFT JOIN import.memoriaal_kivitahvlid kt ON kt.persoon = k.persoon
+    LEFT JOIN import.memoriaal_evo evo ON evo.persoon = k.persoon
+    WHERE k.ekslikkanne = ''
+    AND k.puudulik = ''
+    AND k.allikas NOT IN ('KIVI')
+    -- AND k.persoon = '0000083261'
+    AND k.peatatud = ''
+    -- AND kp.allikas != 'Persoon'
+    AND nk.persoon IS NOT NULL
+    -- AND ks_mr.kirjekood IS NULL
+    AND IFNULL(a.nonPerson, '') != '!'
+    GROUP BY k.persoon
+    HAVING perenimi != ''
+       AND kirjed != ''
+    ;
+
+  END;;
+DELIMITER ;
+
+
+
+-- INTO OUTFILE '/home/michelek/scripts/memoriaal_ee.csv'
+-- FIELDS TERMINATED BY ','
+-- ENCLOSED BY '"'
+-- LINES TERMINATED BY '\n';
